cf99e63d2da71014bf8347a6e846563011214d95
RidvanOzturk <ozturkridvan1@gmail.com>
2023-08-29 09:59:18 +0300

    Tailwind css problem solved.

--- ShoppingCart-ReactJS/src/App.jsx
+++ ShoppingCart-ReactJS/src/App.jsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
     </>
