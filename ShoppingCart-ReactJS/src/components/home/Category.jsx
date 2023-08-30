31aa37ed60ac7e26459c55749e4addbfcb507f3c
RidvanOzturk <ozturkridvan1@gmail.com>
2023-08-30 18:41:28 +0300

    added category api

--- ShoppingCart-ReactJS/src/components/home/Category.jsx
+++ ShoppingCart-ReactJS/src/components/home/Category.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getCategories } from '../../redux/categorySlice';
 
 const Category = () => {
+
+    const dispatch = useDispatch();
+    const {categories} = useSelector(state => state.categories)
+
+    console.log(categories,"categories1");
+
+    useEffect(()=> {
+        dispatch(getCategories())
+    },[dispatch])
   return (
-    <div>Category</div>
+    <div className='w-1/6 bg-gray-200 p-4'>
+        <div className='border-b pb-1 px-2 text-xl font-bold'>CATEGORY</div>
+        {
+            categories?.map((category,i)=>(
+            <div className='text-lg cursor-pointer hover:bg-gray-100 hover:rounded-lg p-2' key={i}>{category}</div>
+            ))
+        }
+    </div>
   )
 }
 
--- ShoppingCart-ReactJS/src/components/home/Sorting.jsx
+++ ShoppingCart-ReactJS/src/components/home/Sorting.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Sorting = () => {
   return (
-    <div className='bg-gray-100 my-3'>Sorting</div>
+    <div className='bg-gray-100 my-5 py-5 flex items-center justify-end'>
+        <select className='bg-white py-3 px-5' name='' id=''>
+            <option disabled value="">Choose one</option>
+            <option  value="inc">Increasing</option>
+            <option  value="dec">decreasing</option>
+        </select>
+    </div>
   )
 }
 
