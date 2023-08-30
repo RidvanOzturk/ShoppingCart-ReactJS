b72998e6a35c697a1b3b22fbf6f61a6f6c76bafa
RidvanOzturk <ozturkridvan1@gmail.com>
2023-08-30 10:03:41 +0300

    Slider bugs done and added category products sorting jsx

--- ShoppingCart-ReactJS/src/components/home/SliderComp.jsx
+++ ShoppingCart-ReactJS/src/components/home/SliderComp.jsx
@@ -1,26 +1,57 @@
-import React from 'react'
-import Slider from "react-slick"
+import React from "react";
+import Slider from "react-slick";
 
 const SliderComp = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-      };
+  const settings = {
+    //dots: true,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     <div>
-        <Slider {...settings}>
-      <div>
-            <div> <img src='https://raw.githubusercontent.com/RidvanOzturk/ShoppingCart-ReactJS/main/ShoppingCart-ReactJS/src/assets/images/nike1.png' alt=''/>
-           </div>
-            <div> <img src='https://raw.githubusercontent.com/RidvanOzturk/ShoppingCart-ReactJS/main/ShoppingCart-ReactJS/src/assets/images/nike2.png' alt=''/>
-           </div>
-      </div>
-        </Slider>
-  </div>
-  )
-}
+      <Slider {...settings}>
+        <div className="!flex items-center bg-gray-100 px-6">
+          <div className="">
+            <div className="text-6xl font-bold">Quality is no coincidence!</div>
+            <div className="text-lg my-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem, sed. Laudantium obcaecati inventore modi libero
+              quos odit ratione vitae ipsum et quae? Quaerat, eius perferendis
+              deserunt explicabo a sapiente delectus?
+            </div>
+            <div className="border rounded-full cursor-pointer text-2xl w-[200px] h-16 flex items-center justify-center bg-gray-200">
+              Review
+            </div>
+          </div>
+          <img
+            src="https://raw.githubusercontent.com/RidvanOzturk/ShoppingCart-ReactJS/main/ShoppingCart-ReactJS/src/assets/images/nike1.png"
+            alt=""
+          />
+        </div>
+        <div className="!flex items-center bg-gray-100 px-6">
+        <div className="">
+            <div className="text-6xl font-bold">Quality is no coincidence!</div>
+            <div className="text-lg my-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Exercitationem, sed. Laudantium obcaecati inventore modi libero
+              quos odit ratione vitae ipsum et quae? Quaerat, eius perferendis
+              deserunt explicabo a sapiente delectus?
+            </div>
+            <div className="border rounded-full cursor-pointer text-2xl w-[200px] h-16 flex items-center justify-center bg-gray-200">
+              Review
+            </div>
+          </div>
+          <img
+            src="https://raw.githubusercontent.com/RidvanOzturk/ShoppingCart-ReactJS/main/ShoppingCart-ReactJS/src/assets/images/nike2.png"
+            alt=""
+          />
+        </div>
+      </Slider>
+    </div>
+  );
+};
 
-export default SliderComp
+export default SliderComp;
---
+++ ShoppingCart-ReactJS/src/components/home/Sorting.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Sorting = () => {
+  return (
+    <div className='bg-gray-100 my-3'>Sorting</div>
+  )
+}
+
+export default Sorting
