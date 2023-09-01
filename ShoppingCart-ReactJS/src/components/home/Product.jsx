68631d2815419ff5db19dbeae30cfacc6accc8ef
RidvanOzturk <ozturkridvan1@gmail.com>
2023-09-01 10:02:00 +0300

    image bug fixed edit css and prices of products added

--- ShoppingCart-ReactJS/src/components/home/Product.jsx
+++ ShoppingCart-ReactJS/src/components/home/Product.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Product = ({product}) => {
   return (
-    <div className='w-[400px] p-2 m-2'>
-        <img className="w-[200px] h-[200px]" src={product?.image} alt=''/>
+    <div className='w-[400px] p-2 m-2 border rounded-md relative'>
+      <div className='text-2xl font-bold absolute rounded-md top-0 right-0 bg-gray-600 text-white p-2 m-1'>{product?.price}<span className='text-sm'>$</span></div>
+        <img className="w-[200px] h-[200px] object-cover m-auto" src={product?.image} alt=''/>
     </div>
   )
 }
