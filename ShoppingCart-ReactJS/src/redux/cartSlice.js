09fbf25a80597357152139451ed72b1008407452
RidvanOzturk <ozturkridvan1@gmail.com>
2023-09-04 20:53:49 +0300

    update cartSlice

--- ShoppingCart-ReactJS/src/redux/cartSlice.js
+++ ShoppingCart-ReactJS/src/redux/cartSlice.js
@@ -1,10 +1,29 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { json } from "react-router-dom";
+
+const fetchFromLocalStorage = () => {
+  let cart = localStorage.getItem("cart");
+  if (cart) {
+    return JSON.parse(localStorage.getItem("cart"));
+  } else {
+    return [];
+  }
+};
+
+const storeInLocalStorage = () => {
+  localStorage.setItem("cart", JSON.stringify(data));
+};
 
 const initialState = {
   cart: fetchFromLocalStorage(),
+  itemCount: 0,
+  totalAmount: 0,
 };
 
 const cartSlice = createSlice({
   name: "cart",
   initialState,
+  reducers: {
+    addToCart: (state, action) => {},
+  },
 });
