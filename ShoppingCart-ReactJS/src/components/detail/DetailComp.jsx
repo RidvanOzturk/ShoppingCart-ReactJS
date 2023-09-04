157c345bb475700c5de57e6ed192bfda9e71bb4a
RidvanOzturk <ozturkridvan1@gmail.com>
2023-09-04 13:28:55 +0300

    added increment and decrement

--- ShoppingCart-ReactJS/src/components/detail/DetailComp.jsx
+++ ShoppingCart-ReactJS/src/components/detail/DetailComp.jsx
@@ -2,6 +2,14 @@ import React, { useState } from "react";
 
 const DetailComp = ({ productDetail }) => {
   const [quantity, setQuantity] = useState(0);
+
+  const decrement = () => {
+    if (quantity > 0 ) setQuantity(quantity-1);
+  }
+  const increment = () => {
+    if (quantity < productDetail?.rating?.count)   setQuantity(quantity+1);
+  }
+
   console.log(productDetail);
   return (
     <div className="flex gap-10 my-10">
@@ -25,13 +33,13 @@ const DetailComp = ({ productDetail }) => {
         </div>
 
         <div className="flex items-center gap-5 my-4 border w-[150px] text-center justify-center rounded-xl">
-          <div className="text-5xl cursor-pointer">-</div>
+          <div onClick={decrement} className="text-5xl cursor-pointer">-</div>
           <input
-            className="w-5 text-center text-4xl font-bold"
+            className="w-11 text-center text-4xl font-bold"
             type="text"
             value={quantity}
           />
-          <div className="text-4xl cursor-pointer">+</div>
+          <div onClick={increment} className="text-4xl cursor-pointer">+</div>
         </div>
         <div className="my-4 border w-[200px] text-2xl bg-gray-200 cursor-pointer h-16 flex items-center justify-center rounded-md">
           Add to Cart
