4f12fab90d2b6527d17c38e1f683204681073ef2
RidvanOzturk <ozturkridvan1@gmail.com>
2023-09-04 13:01:24 +0300

    added title description count price and editted

--- ShoppingCart-ReactJS/src/components/detail/DetailComp.jsx
+++ ShoppingCart-ReactJS/src/components/detail/DetailComp.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const DetailComp = ({productDetail}) => {
+const DetailComp = ({ productDetail }) => {
+  const [quantity, setQuantity] = useState(0);
   console.log(productDetail);
   return (
-    <div>
-        <img src={productDetail?.image} alt='' />
-        <div className=''>
-            <div>
-            {productDetail?.title}  
-            </div>
+    <div className="flex gap-10 my-10">
+      <img
+        className="w-[700px] h-[700px] object-cover"
+        src={productDetail?.image}
+        alt=""
+      />
+      <div className="">
+        <div className="text-4xl font-bold ">{productDetail?.title}</div>
+        <div className="my-2 text-2xl">{productDetail?.description}</div>
+        <div className="my-2 text-xl text-red-500">
+          Rating : {productDetail?.rating?.rate}
         </div>
-    </div>
-  )
-}
+        <div className="my-2 text-xl text-red-500">
+          Count : {productDetail?.rating?.count}
+        </div>
+        <div className="text-5xl font-bold">
+          {productDetail?.price}
+          <span className="text-4xl"> $</span>
+        </div>
 
-export default DetailComp
+        <div className="flex items-center gap-5 my-4 border w-[150px] text-center justify-center rounded-xl">
+          <div className="text-5xl cursor-pointer">-</div>
+          <input
+            className="w-5 text-center text-4xl font-bold"
+            type="text"
+            value={quantity}
+          />
+          <div className="text-4xl cursor-pointer">+</div>
+        </div>
+        <div className="my-4 border w-[200px] text-2xl bg-gray-200 cursor-pointer h-16 flex items-center justify-center rounded-md">
+          Add to Cart
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DetailComp;
--- ShoppingCart-ReactJS/src/pages/Detail.jsx
+++ ShoppingCart-ReactJS/src/pages/Detail.jsx
@@ -1,28 +1,32 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom'
-import { getProductsDetail } from '../redux/productSlice';
-import Loading from "../components/Loading"
-import DetailComp from '../components/detail/DetailComp';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getProductsDetail } from "../redux/productSlice";
+import Loading from "../components/Loading";
+import DetailComp from "../components/detail/DetailComp";
 
 const Detail = () => {
-    const {id}=  useParams();
-    const dispatch = useDispatch(); 
-    const {productDetail, productDetailStatus} = useSelector(state =>state.products)
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { productDetail, productDetailStatus } = useSelector(
+    (state) => state.products
+  );
 
-useEffect(()=>{
-    dispatch(getProductsDetail(id))
-},[dispatch,id])
+  useEffect(() => {
+    dispatch(getProductsDetail(id));
+  }, [dispatch, id]);
 
-console.log(productDetail);
+  console.log(productDetail);
 
   return (
     <div>
-        {
-            productDetailStatus === "LOADING" ? <Loading/> : <DetailComp productDetail={productDetail}/>
-        } 
+      {productDetailStatus === "LOADING" ? (
+        <Loading />
+      ) : (
+        <DetailComp productDetail={productDetail} />
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Detail
+export default Detail;
