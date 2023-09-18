86d381a36e46f6a9eb8464baab47ab5c60c7183f
RidvanOzturk <ozturkridvan1@gmail.com>
2023-09-18 10:59:36 +0300

    added delete button

--- src/components/cart/CartComp.jsx
+++ src/components/cart/CartComp.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const CartComp = ({ cart }) => {
   return (
-    <div className="my-10">
+    <div className="my-10 flex items-center justify-between">
       <img
         className="w-[150px] h-[150px] object-cover"
         src={cart?.image}
@@ -12,6 +12,8 @@ const CartComp = ({ cart }) => {
         <div>{cart?.title}</div>
         <div>{cart?.description}</div>
       </div>
+      <div>{cart?.price} $</div> ({cart?.quantity})
+      <div className="bg-red-500 text-white w-[150px] text-center text-lg rounded-md cursor-pointer h-16 flex items-center justify-center">Delete the Product</div>
     </div>
   );
 };
