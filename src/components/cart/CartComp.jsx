92bb22d8bc982dc98918e1139e0b8fe7dc95306c
RidvanOzturk <ozturkridvan1@gmail.com>
2023-09-20 18:45:59 +0300

    editted code

--- src/components/cart/CartComp.jsx
+++ src/components/cart/CartComp.jsx
@@ -3,7 +3,7 @@ import { removeFromCart } from "../../redux/cartSlice";
 import { useDispatch } from "react-redux";
 
 const CartComp = ({ cart }) => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   return (
     <div className="my-10 flex items-center justify-between">
       <img
@@ -15,8 +15,15 @@ const CartComp = ({ cart }) => {
         <div className="text-xl ">{cart?.title}</div>
         <div>{cart?.description}</div>
       </div>
-      <div className="font-bold text-2xl">{cart?.price} $  ({cart?.quantity})</div>
-      <div onClick={() => dispatch(removeFromCart(cart?.id))} className="bg-red-500 text-white w-[150px] text-center text-lg rounded-md cursor-pointer h-16 flex items-center justify-center">Delete the Product</div>
+      <div className="font-bold text-2xl">
+        {cart?.price} $ ({cart?.quantity})
+      </div>
+      <div
+        onClick={() => dispatch(removeFromCart(cart?.id))}
+        className="bg-red-500 text-white w-[150px] text-center text-lg rounded-md cursor-pointer h-16 flex items-center justify-center"
+      >
+        Delete the Product
+      </div>
     </div>
   );
 };
--- src/components/navbar/navbaritem/NavbarLeft.jsx
+++ src/components/navbar/navbaritem/NavbarLeft.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const NavbarLeft = () => {
   const navigate = useNavigate();
   return (
-    <div onClick={() => navigate("/")} className='text-6xl'>FBR Shopping</div>
-  )
-}
+    <div onClick={() => navigate("/")} className="text-6xl">
+      FBR Shopping
+    </div>
+  );
+};
 
-export default NavbarLeft
+export default NavbarLeft;
